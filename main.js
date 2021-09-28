39735c67ac51fc20d32fe4575c7325c95bdb0463
ivelton de equeiroz <iveltondequeiroz@gmail.com>
2021-09-28 18:55:55 -0400

    flip function

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ Moralis.serverURL = "INSERT_SERVER_URL"; //Server url from moralis.io
 
 async function login() {
     try {
-        currentUser = Moralis.User.current();
-        if(!currentUser){
-            currentUser = await Moralis.Web3.authenticate();
-        }
+        currentUser = await Moralis.Web3.authenticate();
+        console.log(currentUser);
+        document.getElementById("login_button").style.display="none";
+        document.getElementById("game").style.display="block";
     } catch (error) {
         console.log(error);
     }
 }
 
+async function flip(side){
+    alert(side);
+}
+
 document.getElementById("login_button").onclick = login;
+document.getElementById("heads_button").onclick = function(){flip("heads")};
+document.getElementById("tails_button").onclick = function(){flip("tails")};
